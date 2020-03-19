ee97d43dfbe58fbd57782b378caf4cb6b549eba4
C <chrismatusemail@gmail.com>
2020-03-19 00:54:57 +0000

    Loading text now appears on filter button.

--- db/data/development-data/topics.js
+++ db/data/development-data/topics.js
@@ -1,10 +1,10 @@
 module.exports = [
-  { description: "Code is love, code is life", slug: "coding" },
-  { description: "FOOTIE!", slug: "football" },
-  { description: "Hey good looking, what you got cooking?", slug: "cooking" },
   { description: "Please be civil.", slug: "politics" },
   { description: "For all pet related questions", slug: "pets" },
   { description: "Star crossed lovers", slug: "astronomy" },
   { description: "Live. Laugh. Lift.", slug: "fitness" },
-  { description: "stitch me up, scotty", slug: "embroidery" }
+  { description: "stitch me up, scotty", slug: "embroidery" },
+  { description: "Code is love, code is life", slug: "coding" },
+  { description: "FOOTIE!", slug: "football" },
+  { description: "Hey good looking, what you got cooking?", slug: "cooking" }
 ];
