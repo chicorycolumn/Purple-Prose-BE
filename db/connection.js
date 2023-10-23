ed4d0dd20d475753e46610baf2c798ec24d9a087
C <chrismatusemail@gmail.com>
2023-10-23 21:46:31 +0100

    Update knexfile.

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,6 @@
 const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
 
-const dbConfig =
-  ENV === "production"
-    ? { client: "pg", connection: process.env.DATABASE_URL }
-    : require("../knexfile");
+const dbConfig = require("../knexfile");
 
-module.exports = knex(dbConfig);
+module.exports = knex(dbConfig[ENV]);
--- knexfile.js
+++ knexfile.js
@@ -1,31 +1,39 @@
 const { DATABASE_URL } = process.env;
 
 const ENV = process.env.NODE_ENV || "development";
-//This checks the environment choice.
-const baseConfig = {
-  client: "pg",
-  migrations: {
-    directory: "./db/migrations",
-  },
-  //Migrations creates the Schema.
-  seeds: {
-    directory: "./db/seeds",
-  },
-  //Seed populates that schema with data.
-};
+const JWT_SECRET =
+  "3acb898d09f4566cf6b1be2b86c66dc7568729974e211dc66a62b48935fdb8c606ab97eeb14911aa1bef7b83141f051b7d370e9a54bd5e623ad5c5627a8c76f02c91ae9d7d3874b58ba9f0cc15f8739916f0b07a7575932731d03e4625fb7ea36c3842b4b917eaa1d327fe03e429916c8d97a3798c86a91d538c6f1738959028adf17b5082d3dd2649b75d2ff44a7bb4cde7cd2ccc28a2b3e0f1663bcbaad5ef";
 
-// And this below is what figures the environment choice.
-const customConfig = {
+module.exports = {
   production: {
-    connection: `${DATABASE_URL}?ssl=true`,
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: __dirname + "/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds",
+    },
   },
+
   development: {
+    client: "pg",
     connection: {
       database: "nc_news",
       user: "postgres",
       password: "$$PASSWORD",
     },
+    migrations: {
+      directory: __dirname + "/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds",
+    },
   },
+
   test: {
     connection: {
       database: "nc_news_test",
@@ -33,10 +41,9 @@ const customConfig = {
       password: "$$PASSWORD",
     },
   },
+
+  JWT_SECRET,
 };
 
-const JWT_SECRET =
-  "3acb898d09f4566cf6b1be2b86c66dc7568729974e211dc66a62b48935fdb8c606ab97eeb14911aa1bef7b83141f051b7d370e9a54bd5e623ad5c5627a8c76f02c91ae9d7d3874b58ba9f0cc15f8739916f0b07a7575932731d03e4625fb7ea36c3842b4b917eaa1d327fe03e429916c8d97a3798c86a91d538c6f1738959028adf17b5082d3dd2649b75d2ff44a7bb4cde7cd2ccc28a2b3e0f1663bcbaad5ef";
-
-//Here we export which data set based on environment choice.
-module.exports = { ...customConfig[ENV], ...baseConfig, JWT_SECRET };
+// //Here we export which data set based on environment choice.
+// module.exports = { ...customConfig[ENV], ...baseConfig, JWT_SECRET };
